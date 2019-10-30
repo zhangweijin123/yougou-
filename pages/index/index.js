7f09a71f83fcffc8ccfc8f4664391a1f0b1efe6b
zhangweijin123 <615745978@qq.com>
2019-10-30 15:36:35 +0800

    菜单列表完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,20 +2,34 @@
 import request from "../../utils/request.js"
 Page({
   data: {
+    // 轮播图列表数据
     background:[],
-    indicatorDots: true,
-    autoplay: true,
-    interval: 2000,
-    duration: 500
+
+    //菜单列表数据
+    menus:[]
   },
 
+
+
   onLoad(){
+    //请求轮播图数据
     request({
       url:"/api/public/v1/home/swiperdata"
     }).then(res=>{
       const {message} = res.data;
       this.setData({
         background:message
+      })
+    })
+
+    //请求菜单列表数据
+    request({
+      url:"/api/public/v1/home/catitems"
+    }).then(res=>{
+      console.log(res)
+      const {message} = res.data;
+      this.setData({
+        menus:message
       })
     })
   }

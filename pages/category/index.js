b6d2e79348f7e089a5135ab8bb3ca1053918d9e5
zhangweijin123 <615745978@qq.com>
2019-10-30 21:41:10 +0800

    左侧菜单高亮显示

--- pages/category/index.js
+++ pages/category/index.js
@@ -5,62 +5,16 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+      //当前选中的索引 默认初始为0
+      current:0
   },
+  //点击左侧菜单时候出发
+  handleChange(event){
+    //获取index
+    const {index} = event.target.dataset;
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    this.setData({
+      current : index
+    })
   }
 })

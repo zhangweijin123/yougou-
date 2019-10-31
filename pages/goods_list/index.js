0c3f2f50f4dec73eee3300cbd514abacdafd9d99
zhangweijin123 <615745978@qq.com>
2019-10-31 16:05:10 +0800

    请求商品列表数据完成

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,18 +1,47 @@
 // pages/goods_list/index.js
+import request from "../../utils/request.js"
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+      //url搜索关键字 分类页传过来的
+      query:"",
+      //商品列表 请求回来的数据
+      goods:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // options是页面的传参数，比如 {query: "曲面电视"}
+    const { query } = options;
+    this.setData({
+      query
+    });
+      request({
+        url:"/api/public/v1/goods/search",
+        data:{
+          query,
+          pagenum:1,
+          pagesize:10
+        }
+      }).then(res=>{
+        console.log(res)
+        const {goods} = res.data.message;
 
+        // 循环给每个商品价格保留两位小数点
+        const newGoods = goods.map(v=>{
+          v.goods_price = Number(v.goods_price).toFixed(2);
+          return v;
+        })
+
+        this.setData({
+          goods
+        })
+      })
   },
 
   /**

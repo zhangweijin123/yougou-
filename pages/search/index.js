5bf670c36eda93b57e6aed50ec7f1306d1748660
zhangweijin123 <615745978@qq.com>
2019-11-01 16:32:40 +0800

    判断输入框内是否有值来控制取消按钮

--- pages/search/index.js
+++ pages/search/index.js
@@ -5,62 +5,31 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+      //判断是否点击取消按钮
+      isShow:false,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    
+  },
+  //监听输入框输值得时候事件
+  handleInput(event){
+      //获取输入框的值注意wxml中的事件是brindinput=handleInput
+      const {value} = event.detail
+      // 声明isShow变量
+      let isShow;
+
+    //判断输入框里面有没有值 trim()去除前后空格
+    //如果输入框里面有值 那么就是true显示取消按钮 则反之
+    isShow = value.trim() ? true : false
+    // 改变isShow的状态
+    this.setData({
+      isShow
+    })
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+  
 })

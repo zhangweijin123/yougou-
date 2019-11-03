23d2f1f73f7c0acbde383246e42ac556ad7e4341
zhangweijin123 <615745978@qq.com>
2019-11-03 16:52:07 +0800

    收货地址布局以及获取地址

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -5,62 +5,27 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    //地址接收对象
+    address:{}
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //获取收货地址
+  handleAddress(){
+    wx.chooseAddress({
+      success:(res)=> {
+        // console.log(res.userName)
+        // console.log(res.postalCode)
+        // console.log(res.provinceName)
+        // console.log(res.cityName)
+        // console.log(res.countyName)
+        // console.log(res.detailInfo)
+        // console.log(res.nationalCode)
+        // console.log(res.telNumber)
+      this.setData({
+        userName: res.userName,
+        telNumber: res.telNumber,
+        detail: res.provinceName + res.cityName + res.countyName + res.detailInfo
+      })
+      }
+    })
   }
 })
